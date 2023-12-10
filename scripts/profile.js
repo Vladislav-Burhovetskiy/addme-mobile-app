dea8cb2ad2c8120aa7ac60f46da0e690002cfc45
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2023-12-10 19:14:23 +0200

    Fixed path for default user photo after sign in

--- scripts/profile.js
+++ scripts/profile.js
@@ -173,7 +173,7 @@ function showProfilePicture(imgElement, user) {
   if (photoURL) {
     imgElement.src = photoURL;
   } else {
-    imgElement.src = "assets/images/default-profile-picture.jpeg";
+    imgElement.src = "../assets/images/default-profile-picture.jpeg";
   }
 }
 
